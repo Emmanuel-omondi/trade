14f18bf2826a09f78acd06123881ed15d89a373a
Emmanuel-omondi <128595687+Emmanuel-omondi@users.noreply.github.com>
2026-06-08 09:46:22 +0300

    Delete forex_ai directory

--- forex_ai/build_feature_engine.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-/c/Users/Admin/Desktop/forex_ai/core/engine/feature_engine.c
-"/c/msys64/mingw64/bin/gcc.exe" -O3 -shared -o /c/Users/Admin/Desktop/forex_ai/core/engine/feature_engine.dll -fPIC /c/Users/Admin/Desktop/forex_ai/core/engine/feature_engine.c
--- forex_ai/install_msys2_toolchain.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\msys64\usr\bin\bash.exe" -lc "pacman --noconfirm -Syu mingw-w64-x86_64-toolchain"
